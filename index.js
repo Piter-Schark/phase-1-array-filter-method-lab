5f8e41dde0c68335a075a23156ed41f7940b0eb5
Peter <pschoenbauer@gmail.com>
2023-04-13 21:46:30 -0400

    array filter lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(drivers, placeholder){
+    const result = drivers.filter(driversName => driversName.toLowerCase() === placeholder.toLowerCase());
+    return result; 
+    }
+
+function fuzzyMatch(drivers, placeholder){
+    const result = drivers.filter(driversName => driversName.startsWith(placeholder))
+    return result;
+}
+function matchName(drivers, string) {
+    const result = drivers.filter(driversName => driversName.name === string)
+    return result;
+}
